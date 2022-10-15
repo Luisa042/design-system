f56c7f22525ab0b845de0e0fa49c812a2a0253b9
Luisa <luisa.santos042@gmail.com>
2022-10-15 12:33:22 +0000

    create interface, make adjustments
    create demo interface, adjust elements, create logo element

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,11 +1,11 @@
 import * as CheckboxBase from '@radix-ui/react-checkbox'
 import { Check } from 'phosphor-react'
 
-export interface CheckboxProps {}
+export interface CheckboxProps extends CheckboxBase.CheckboxProps {}
 
-export function Checkbox({}: CheckboxProps) {
+export function Checkbox(props: CheckboxProps) {
     return (
-        <CheckboxBase.Root className='w-6 h-6 p-[2px] bg-gray-800 rounded'>
+        <CheckboxBase.Root className='w-6 h-6 p-[2px] bg-gray-800 rounded' {...props}>
             <CheckboxBase.Indicator asChild>
                 <Check weight='bold' className='w-5 h-5 text-cyan-500' />
             </CheckboxBase.Indicator>
